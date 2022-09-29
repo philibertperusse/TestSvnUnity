d6c7bb772436612e0b7b2016c7e096a7dbd43973
beap07 <113565988+beap07@users.noreply.github.com>
2022-09-29 12:22:21 -0400

    sprint character ne fonctionne pas encore

---
+++ Assets/Scripts/PlayerScripts/PlayerController/CharacterMovement.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] private float _speed = 1;
+
+    private void Update()
+    {
+        var dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        transform.Translate(dir * _speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerScripts/PlayerController/CharacterSprint.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSprint : MonoBehaviour
+{
+
+    float _speed = 5f;
+    float dirX;
+   
+        
+        private void Uptade()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+            _speed = 10f;
+        else
+            _speed = 5f;
+
+        dirX = Input.GetAxis("Horizontal") + _speed;
+    }
+}
