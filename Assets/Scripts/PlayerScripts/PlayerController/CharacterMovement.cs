e02bdb0666a14fe1e2dc504faca9050aadad0805
beap07 <113565988+beap07@users.noreply.github.com>
2022-10-06 12:21:16 -0400

    Control du perso.

--- Assets/Scripts/PlayerScripts/PlayerController/CharacterMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerController/CharacterMovement.cs
@@ -4,14 +4,37 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-    [SerializeField] private float _speed = 1;
 
-    private void Update()
+    [SerializeField] private float _Speed;
+    [SerializeField] private float _runningSpeed;
+    [SerializeField] private float jumpforce;
+    private bool _IsGrounded;
+
+    void Update()
     {
-        var dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        transform.Translate(dir * _speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Translate(_Speed * Time.deltaTime, 0, 0);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Translate(-_Speed * Time.deltaTime, 0, 0);
+
+        if ((Input.GetKey(KeyCode.LeftShift)) && (Input.GetKey(KeyCode.RightArrow)))
+            transform.Translate(_Speed * _runningSpeed * Time.deltaTime, 0, 0);
+
+        if ((Input.GetKey(KeyCode.LeftShift)) && (Input.GetKey(KeyCode.LeftArrow)))
+            transform.Translate(-_Speed * _runningSpeed * Time.deltaTime, 0, 0);
+
+        if (Input.GetKey(KeyCode.Space))
+            transform.Translate(0, jumpforce * Time.deltaTime, 0);
+
+        if ((Input.GetKey(KeyCode.Space)) && (Input.GetKey(KeyCode.RightArrow)))
+            transform.Translate(_Speed * Time.deltaTime, jumpforce * Time.deltaTime, 0);
+
+        if ((Input.GetKey(KeyCode.Space)) && (Input.GetKey(KeyCode.LeftArrow)))
+            transform.Translate(-_Speed * Time.deltaTime, jumpforce * Time.deltaTime, 0);
+
+
+
     }
-
-
-
 }
+
