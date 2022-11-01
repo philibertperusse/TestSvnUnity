e57de018dd7b72e4f320b93892240d96060a5c09
Patidou0 <113621613+Patidou0@users.noreply.github.com>
2022-11-01 12:07:06 -0400

    AJout dans la scene trois du script de nuage et de pétales

---
+++ Assets/CloudMovement.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMovement : MonoBehaviour
+{
+    [SerializeField] private float _Speed;
+
+    
+    void Update()
+    {
+        transform.Translate(_Speed * Time.deltaTime, 0, 0);
+    }
+}
---
+++ Assets/PetalMovement.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PetalMovement : MonoBehaviour
+{
+    [SerializeField] private float _Speed;
+    void Update()
+    {
+        transform.Translate(-_Speed * Time.deltaTime, 0, 0);
+    }
+}
