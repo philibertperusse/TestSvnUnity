d1f37309d5d27e9210d062865722469f6bd13462
beap07 <113565988+beap07@users.noreply.github.com>
2022-11-24 11:35:34 -0500

    rotation des pétales

--- Assets/CharacterRotation.cs
+++ Assets/CharacterRotation.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterRotation : MonoBehaviour
-{
-    private float zRot = 45;
-    public Transform playerObj;
 
-    void FixedUpdate()
-    {
-        zRot = Mathf.Clamp(45, -25, 25);
-    }
+public class CharacterRotation : MonoBehaviour
+{   
+    
+    private float zRot = 45;
+public Transform playerObj;
+
+void FixedUpdate()
+{
+    zRot = Mathf.Clamp(45, -25, 25);
+}
 }
--- Assets/PetalMovement.cs
+++ Assets/PetalMovement.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class PetalMovement : MonoBehaviour
 {
+   
     [SerializeField] private float _Speed;
     void Update()
     {
         transform.Translate(-_Speed * Time.deltaTime, 0, 0);
+    
+    //mouvement des pétales vers la gauche
+    
+    
+        transform.Rotate(0, 0, 50 * Time.deltaTime);
+        //rotates 50 degrees per second around z axis
     }
-
-        spriteRenderer.flip.x = true;
-        spriteRenderer.flip.x = false;
-
 
 
 }
