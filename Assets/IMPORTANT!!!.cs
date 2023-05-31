be84a1be6e74d232ba67dc7c284c36e9e6dad616
Patidou0 <113621613+Patidou0@users.noreply.github.com>
2023-05-31 12:00:53 -0400

    ajout a la liste important

--- Assets/IMPORTANT!!!.cs
+++ Assets/IMPORTANT!!!.cs
@@ -1,5 +1,12 @@
-// petit pique sur arbre,dessiner les block qui tombe
+// petit pique sur arbre
+// dessiner les block qui tombe
 // version plus petite des sol boueux
 // sol pour tron d'arbre couper,aussi pour les tas de feuille et des arbre normal
 // design du personage
 // visuel des tornade  
+// finir block boue
+// coin top en haut
+// decomposition fleur
+// anime attack sol et air
+// tout mouvement gauche,crouch et courir
+// background niv 1,2,3, sous sol 3, boss
